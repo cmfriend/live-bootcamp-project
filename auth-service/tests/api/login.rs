d85828e4fc9ab3a299ee9e2934738f005212ee24
Chris Friend <cmfriend@gmail.com>
2026-01-19 11:53:43 -0500

    [Sprint 1][Task 6] Implement sign-up route interface

--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,7 +1,7 @@
 use crate::helpers::TestApp;
 
 #[tokio::test]
-async fn login_returns_success() {
+async fn login_should_return_200() {
     let app = TestApp::new().await;
 
     let response = app.post_login().await;
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,10 +1,70 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
-async fn signup_returns_success() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
-    let response = app.post_signup().await;
+    let random_email = get_random_email();
 
-    assert_eq!(response.status().as_u16(), 200);
+    let test_cases = [
+        serde_json::json!({}),
+        serde_json::json!({
+            "invalidfield": null,
+        }),
+        serde_json::json!({
+            "invalidfield": "invalidvalue",
+        }),
+        serde_json::json!({
+            "email": null,
+        }),
+        serde_json::json!({
+            "email": random_email,
+        }),
+        serde_json::json!({
+            "password": null
+        }),
+        serde_json::json!({
+            "password": "password123"
+        }),
+        serde_json::json!({
+            "requires2FA": null,
+        }),
+        serde_json::json!({
+            "requires2FA": false,
+        }),
+        serde_json::json!({
+            "email": null,
+            "password": null,
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": "password123",
+        }),
+        serde_json::json!({
+            "password": null,
+            "requires2FA": null,
+        }),
+        serde_json::json!({
+            "password": "password123",
+            "requires2FA": true,
+        }),
+        serde_json::json!({
+            "email": null,
+            "requires2FA": null,
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "requires2FA": true,
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
