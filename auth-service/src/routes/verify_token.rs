2d3da1c3ef0e00c45b0946562469ab15a808b548
Chris Friend <cmfriend@gmail.com>
2026-02-06 21:42:22 -0500

    [Sprint 3][Task 4] Implement verify-token route

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,8 +1,22 @@
 use axum::{
     http::StatusCode,
     response::IntoResponse,
+    Json,
 };
 
-pub async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{
+    domain::AuthAPIError,
+    utils::auth::validate_token
+};
+use serde::Deserialize;
+
+pub async fn verify_token(Json(request): Json<VerifyTokenRequest>) -> Result<impl IntoResponse, AuthAPIError> {
+    let _ = validate_token(&request.token).await.map_err(|_| AuthAPIError::InvalidToken)?;
+
+    Ok(StatusCode::OK)
+}
+
+#[derive(Deserialize)]
+pub struct VerifyTokenRequest {
+    pub token: String,
 }
